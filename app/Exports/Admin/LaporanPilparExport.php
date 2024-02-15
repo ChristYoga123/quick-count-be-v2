d212a07e3be22cca49dae28da6fd9ed7cc8c7a2d
ChristYoga123 <christianuswibisono@gmail.com>
2024-02-15 20:32:45 +0700

    [FIX] fix laporan

--- app/Exports/Admin/LaporanPilparExport.php
+++ app/Exports/Admin/LaporanPilparExport.php
@@ -3,11 +3,12 @@
 namespace App\Exports\Admin;
 
 use App\Models\Dapil;
+use App\Models\Partai;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class LaporanPilparExport implements FromCollection, WithHeadings
+class LaporanPilparExport implements FromCollection
 {
     public Dapil $dapil;
 
@@ -20,16 +21,31 @@ class LaporanPilparExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        return DB::table('laporan_pilpars')
-            ->select('laporan_pilpars.id', 'users.name', 'laporan_pilpars.laporan')
+        $realCountPartai = DB::table('suara_pilpars')
+            ->select('users.email', 'users.name', 'dapils.index', 'pilpars.kelurahan', 'pilpars.tps', 'pilpars.jumlah_dpt', 'pilpars.hasil_suara_tidak_sah', 'suara_pilpars.partai_id', 'suara_pilpars.jumlah_suara', 'pilpars.id as pilpar_id')
+            ->join('laporan_pilpars', 'suara_pilpars.pilpar_id', '=', 'laporan_pilpars.pilpar_id')
+            ->join('pilpars', 'suara_pilpars.pilpar_id', '=', 'pilpars.id')
             ->join('users', 'laporan_pilpars.user_id', '=', 'users.id')
-            ->join('pilpars', 'laporan_pilpars.pilpar_id', '=', 'pilpars.id')
+            ->join('dapils', 'pilpars.dapil_id', '=', 'dapils.id')
             ->where('pilpars.dapil_id', $this->dapil->id)
             ->get();
-    }
 
-    public function headings(): array
-    {
-        return ['Id', 'Nama', 'Laporan'];
+        $realCountPartai = $realCountPartai->reduce(function ($carry, $item) {
+            if (!isset($carry[$item->pilpar_id])) {
+                $carry[$item->pilpar_id] = [
+                    'email' => $item->email,
+                    'name' => $item->name,
+                    'index' => $item->index,
+                    'kelurahan' => $item->kelurahan,
+                    'tps' => $item->tps,
+                    'jumlah_dpt' => $item->jumlah_dpt,
+                    'hasil_suara_tidak_sah' => $item->hasil_suara_tidak_sah
+                ];
+            }
+            $carry[$item->pilpar_id][$item->partai_id] = $item->jumlah_suara;
+            return $carry;
+        }, []);
+
+        return collect($realCountPartai);
     }
 }
--- app/Exports/Admin/LaporanPilpresExport.php
+++ app/Exports/Admin/LaporanPilpresExport.php
@@ -20,41 +20,31 @@ class LaporanPilpresExport implements FromCollection
      */
     public function collection()
     {
-        $realCountPresiden = DB::table('laporan_pilpres')
-            ->select('users.email', 'users.name', 'dapils.index', 'pilpres.kelurahan', 'pilpres.tps', 'pilpres.jumlah_dpt', 'pilpres.hasil_suara_tidak_sah')
+        $realCountPresiden = DB::table('suara_pilpres')
+            ->select('users.email', 'users.name', 'dapils.index', 'pilpres.kelurahan', 'pilpres.tps', 'pilpres.jumlah_dpt', 'pilpres.hasil_suara_tidak_sah', 'suara_pilpres.capres_id', 'suara_pilpres.jumlah_suara', 'pilpres.id as pilpres_id')
+            ->join('laporan_pilpres', 'suara_pilpres.pilpres_id', '=', 'laporan_pilpres.pilpres_id')
+            ->join('pilpres', 'suara_pilpres.pilpres_id', '=', 'pilpres.id')
             ->join('users', 'laporan_pilpres.user_id', '=', 'users.id')
-            ->join('pilpres', 'laporan_pilpres.pilpres_id', '=', 'pilpres.id')
             ->join('dapils', 'pilpres.dapil_id', '=', 'dapils.id')
             ->where('pilpres.dapil_id', $this->dapil->id)
             ->get();
 
-        $suaraSah = DB::table('suara_pilpres')
-            ->select('capres.no_urut_paslon', 'suara_pilpres.jumlah_suara', 'pilpres.kelurahan', 'pilpres.tps')
-            ->join('capres', 'suara_pilpres.capres_id', '=', 'capres.id')
-            ->join('pilpres', 'suara_pilpres.pilpres_id', '=', 'pilpres.id')
-            ->where('pilpres.dapil_id', $this->dapil->id)
-            ->get();
-
-        foreach ($realCountPresiden as $key => $value) {
-            $suaraSahNoUrut1 = 0;
-            $suaraSahNoUrut2 = 0;
-            $suaraSahNoUrut3 = 0;
-            foreach ($suaraSah as $key2 => $value2) {
-                if ($value->kelurahan == $value2->kelurahan && $value->tps == $value2->tps) {
-                    if ($value2->no_urut_paslon == 1) {
-                        $suaraSahNoUrut1 = $value2->jumlah_suara;
-                    } elseif ($value2->no_urut_paslon == 2) {
-                        $suaraSahNoUrut2 = $value2->jumlah_suara;
-                    } elseif ($value2->no_urut_paslon == 3) {
-                        $suaraSahNoUrut3 = $value2->jumlah_suara;
-                    }
-                }
+        $realCountPresiden = $realCountPresiden->reduce(function ($carry, $item) {
+            if (!isset($carry[$item->pilpres_id])) {
+                $carry[$item->pilpres_id] = [
+                    'email' => $item->email,
+                    'name' => $item->name,
+                    'index' => $item->index,
+                    'kelurahan' => $item->kelurahan,
+                    'tps' => $item->tps,
+                    'jumlah_dpt' => $item->jumlah_dpt,
+                    'hasil_suara_tidak_sah' => $item->hasil_suara_tidak_sah
+                ];
             }
-            $realCountPresiden[$key]->suara_sah_no_urut_1 = $suaraSahNoUrut1;
-            $realCountPresiden[$key]->suara_sah_no_urut_2 = $suaraSahNoUrut2;
-            $realCountPresiden[$key]->suara_sah_no_urut_3 = $suaraSahNoUrut3;
-        }
+            $carry[$item->pilpres_id][$item->capres_id] = $item->jumlah_suara;
+            return $carry;
+        }, []);
 
-        return $realCountPresiden;
+        return collect($realCountPresiden);
     }
 }
